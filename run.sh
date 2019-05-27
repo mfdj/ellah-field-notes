1f8d3d6c0a17f31ed9c8f633d05e8735bb343f3a
Mark Fox <mf@ixel.org>
2019-05-27 16:29:43 -0700

    test runner: add ability to pass arguments to test.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
+#
+# • finds a directory which matches $1
+# • cd's into that directory and executes ./test.sh with the remaining arguments
+#
 
-find_exec() {
-   cd "$0" && {
+dir_name_fragment=$1
+shift 1
+
+exec_test() {
+   local dir_name
+   dir_name=$1
+   shift 1
+
+   cd "$dir_name" && {
       echo -n '💨 '
-      tr -d './' <<< "$0"
-      ./test.sh
+      tr -d './' <<< "$dir_name"
+      ./test.sh "$@"
    }
 }
-export -f find_exec
+export -f exec_test
 
-find . -depth 1 -type d -iname "*${1}*" -exec bash -c find_exec {} \;
+find . -depth 1 -type d -iname "*${dir_name_fragment}*" -exec bash -c 'exec_test "$@"' _ {} "$@" \;

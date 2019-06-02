406e37ae6e84b2ca6f6000e0727799c9017130d1
Mark Fox <mf@ixel.org>
2019-06-02 16:59:30 -0700

    resolve-path-of-script: added rbenv_abs_dirname; refactored significantly the runner

--- resolve-path-of-script/test-resolve-script-dir-poc.sh
+++ resolve-path-of-script/test-resolve-script-dir-poc.sh
@@ -7,13 +7,13 @@ temp_dir=$(mktemp -d)
 script='test script'
 conrete_dir='demo directory'
 rm -rf "$temp_dir:?/"*
-expected_temp_dir="$(cd "$temp_dir" && pwd -P)/$conrete_dir"
+default_expectation="$(cd "$temp_dir" && pwd -P)/$conrete_dir"
 
-test() {
+assert() {
    local message actual expected
-   message=$1
+   message="$1"
    actual="$(bash "$2")"
-   expected=$3
+   expected="${3:-$default_expectation}"
 
    if [ "$actual" = "$expected" ]; then
       echo -e "\033[32m✔︎ Tested $message"
@@ -25,77 +25,120 @@ test() {
    echo -en "\033[0m"
 }
 
-test_suite() {
-   test 'absolute call'          "$temp_dir/$conrete_dir/$script.sh"                  "$expected_temp_dir"
-   test 'via symlinked dir'      "$temp_dir/$conrete_dir-symlink/$script.sh"          "$expected_temp_dir"
-   test 'via symlinked file'     "$temp_dir/$script-symlink.sh"                       "$expected_temp_dir"
-   test 'via multiple symlinks'  "$temp_dir/$conrete_dir-symlink/loop/$script.sh"     "$expected_temp_dir"
-   test 'symlink script + dir'   "$temp_dir/$conrete_dir-symlink/$script-symlink.sh"  "$expected_temp_dir"
+assertions() {
+   assert 'absolute call'          "$temp_dir/$conrete_dir/$script.sh"
+   assert 'via symlinked dir'      "$temp_dir/$conrete_dir-symlink/$script.sh"
+   assert 'via symlinked file'     "$temp_dir/$script-symlink.sh"
+   assert 'via multiple symlinks'  "$temp_dir/$conrete_dir-symlink/loop/$script.sh"
+   assert 'symlink script + dir'   "$temp_dir/$conrete_dir-symlink/$script-symlink.sh"
    pushd "$temp_dir" > /dev/null && {
-      test 'relative call'       "./$conrete_dir/$script.sh"                          "$expected_temp_dir"
+      assert 'relative call'       "./$conrete_dir/$script.sh"
    }
    echo
 }
 
 setup() {
    mkdir "$temp_dir/$conrete_dir"
+   touch "$temp_dir/$conrete_dir/$script.sh"
 
-   ln -s "$temp_dir/$conrete_dir"             "$temp_dir/$conrete_dir-symlink"
-   ln -s "$temp_dir/$conrete_dir"             "$temp_dir/$conrete_dir-symlink/loop"
-   ln -s "$temp_dir/$conrete_dir/$script.sh"  "$temp_dir/$script-symlink.sh"
-   ln -s "$temp_dir/$conrete_dir/$script.sh"  "$temp_dir/$conrete_dir-symlink/$script-symlink.sh"
+   ln -s  "$temp_dir/$conrete_dir"             "$temp_dir/$conrete_dir-symlink"
+   ln -s  "$temp_dir/$conrete_dir"             "$temp_dir/$conrete_dir-symlink/loop"
+   ln -s  "$temp_dir/$conrete_dir/$script.sh"  "$temp_dir/$script-symlink.sh"
+   ln -s  "$temp_dir/$conrete_dir/$script.sh"  "$temp_dir/$conrete_dir-symlink/$script-symlink.sh"
 
+   echo
    tree "$temp_dir"
+   echo
 }
 
+test_function() {
+   local funcname
+   funcname="$1"
+
+   if ! type -t "$funcname" | grep "function" &> /dev/null; then
+      echo "'$funcname' is not a function"
+      return 1
+   fi
+
+   echo "Test: $funcname"
+   type "$funcname" | tail -n+2  > "$temp_dir/$conrete_dir/$script.sh"
+   echo "$funcname"             >> "$temp_dir/$conrete_dir/$script.sh"
+
+   assertions
+}
+
+#
+# • `dirname $0` truncates the script file from the path expression (recall,
+#   dirname is essentially a string utility)
+# • `cd -P` changes into that directory, resolving any symlinks to their
+#    physical path
+# • `&& pwd` if the cd is successful will print the absolute working directory
+#
 cd_dirname_pwd() {
-   echo 'Test via pwd'
-   cat <<- 'EOF' > "$temp_dir/$conrete_dir/$script.sh"
-      current_dir="$(cd -P "$(dirname "$0")" && pwd)"
-      echo "$current_dir"
-EOF
-   test_suite
+   current_dir="$(cd -P "$(dirname "$0")" && pwd)"
+   echo "$current_dir"
 }
 
-complicated_so_solution() {
-   echo 'Test complicated stackoverflow solution'
-   cat <<- 'EOF' > "$temp_dir/$conrete_dir/$script.sh"
-      currentfile="$0"
-      while [ -h "$currentfile" ]; do
-         currentdir="$(cd -P "$(dirname "$currentfile")" && pwd)"
-         currentfile="$(readlink "$currentfile")"
-         [[ $currentfile != /* ]] && currentfile="$currentdir/$currentfile"
-      done
-      finaldir="$(cd -P "$(dirname "$currentfile")" && pwd)"
-      echo "$finaldir"
-EOF
-   test_suite
+#
+# • …
+#
+readlink_loop() {
+   currentfile="$0"
+   while [ -h "$currentfile" ]; do
+      currentdir="$(cd -P "$(dirname "$currentfile")" && pwd)"
+      currentfile="$(readlink "$currentfile")"
+      [[ $currentfile != /* ]] && currentfile="$currentdir/$currentfile"
+   done
+   finaldir="$(cd -P "$(dirname "$currentfile")" && pwd)"
+   echo "$finaldir"
 }
 
+#
+# • …
+#
 dirname_readlink_0() {
-   echo 'Test via readlink'
-   cat <<- 'EOF' > "$temp_dir/$conrete_dir/$script.sh"
-      readlink_program=$(which greadlink readlink | head -n1)
-      dirname "$("$readlink_program" -f "$0")"
-EOF
-   test_suite
+   readlink_program=$(which greadlink readlink | head -n1)
+   dirname "$("$readlink_program" --canonicalize "$0")"
 }
 
-cd_dirname_pwd() {
-   echo 'Test via pwd'
-   cat <<- 'EOF' > "$temp_dir/$conrete_dir/$script.sh"
-      current_dir="$(cd -P "$(dirname "$0")" && pwd)"
-      echo "$current_dir"
-EOF
-   test_suite
+#
+# • adapted from https://github.com/rbenv/rbenv/blob/master/libexec/rbenv#L34-L50
+# • inlined resolve_link to ease integeration with this test runner (doesn't change behavior meaningfully)
+# • added `cd … || return 1` because this script doesn't use `set -e`
+# • Added `-P` to `cd -P "${path%/*}"` because it's the result we expect (and is essentially more accurate)
+#
+rbenv_abs_dirname() {
+   local cwd="$PWD"
+   local path="$0"
+
+   RBENV_READLINK=$(type -p greadlink readlink | head -1)
+   rbenv_resolve_link() {
+      "$RBENV_READLINK" "$1"
+   }
+
+   while [ -n "$path" ]; do
+      cd -P "${path%/*}" || return 1
+      local name="${path##*/}"
+      path="$(rbenv_resolve_link "$name" || true)"
+   done
+
+   pwd
+   cd "$cwd" || return 1
 }
 
-echo
 setup
+
 if [ "$1" != "" ]; then
-   $1
+   test_function "$1"
 else
-   cd_dirname_pwd
-   complicated_so_solution
-   dirname_readlink_0
+   all_functions=(
+      cd_dirname_pwd
+      readlink_loop
+      dirname_readlink_0
+      rbenv_abs_dirname
+   )
+
+   for func in "${all_functions[@]}"; do
+      test_function "$func"
+   done
 fi

01fc9ddc01d2fce57b5c0cbcf01d75aed218b913
Mark Fox <mf@ixel.org>
2019-06-11 22:58:34 -0700

    resolve-path-of-script: look at dirname v. string-substitution

--- resolve-path-of-script/test-dirname.sh
+++ resolve-path-of-script/test-dirname.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 
+# Test A: basic usage
+
 in_another_directory=$(mktemp -d)/in-another-directory
 cat <<- 'EOF' >"$in_another_directory"
    echo "$0"
    dirname "$0"
+   echo "${0%/*}" # string substituton method means one less subshell
 EOF
 bash "$in_another_directory"
 rm "$in_another_directory"
+
+# Test B: comparing dirname and string-substituton
+
+compare_dirname_stringsub() {
+   echo "--- '$1'"
+   dirname "$1"
+   echo "${1%/*}"
+}
+
+compare_dirname_stringsub .
+compare_dirname_stringsub ./
+compare_dirname_stringsub /asdf
+compare_dirname_stringsub /asdf/\ /
+compare_dirname_stringsub /asdf///
+compare_dirname_stringsub asdf/ghjk
+compare_dirname_stringsub ./asdf/ghjk

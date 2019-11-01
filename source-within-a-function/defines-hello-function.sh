41694aca2e4a9f1263a098aea8ee4a59a78b14bd
Mark Fox <mf@ixel.org>
2019-10-31 23:52:25 -0700

    when we source a file which defines a function from within a function - guess what - the source'd functions is available throughout the scope that the sourcing function is defined in

---
+++ source-within-a-function/defines-hello-function.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+hello() {
+   echo 'just saying hello'
+}
---
+++ source-within-a-function/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source_hello() {
+   source ./defines-hello-function.sh
+}
+
+uses_hello() {
+   hello
+}
+
+type hello 2>&1 | grep -q 'type: hello: not found'
+assert_return_code 0 'hello is not a function yet'
+source_hello
+
+type hello 2>&1 | grep -q 'is a function'
+assert_return_code 0 'hello is a function'
+
+uses_hello
